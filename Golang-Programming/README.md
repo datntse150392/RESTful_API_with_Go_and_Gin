f71ce37624101ca8041ba0e1566331de6f0939b5
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-01-25 01:14:27 +0700

    ✨Lập trình Golang #4: Arrays

--- Golang-Programming/README.md
+++ Golang-Programming/README.md
@@ -236,3 +236,19 @@ func main() {
 	fmt.Println(z.X)
 }
 ```
+
+### ARRAYS
+
+```
+package main
+
+import "fmt"
+
+func main() {
+	var a [2]string // Biến a là mảng string có 2 phần tử
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Print(a)
+}
+```
--- Golang-Programming/my-app/main.go
+++ Golang-Programming/my-app/main.go
@@ -2,30 +2,10 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X int
-	Y int
-}
-
-var(
-	v1 = Vertex{1, 2} // has type Vertex
-	v2 = Vertex{X: 1} // Y:0 is implicit
-	v3 = Vertex{} // X:0 and Y:0
-	p = &Vertex{1, 2} // has type *Vertex
-)
-
 func main() {
-
-	v := Vertex{1, 2}
-	v.X = 4
-
-	fmt.Println(v.X)
-
-	// Using piointer
-	p:= &v // Dùng con trỏ p trỏ đến ô nhớ địa chỉ của v
-	p.X = 10 // Thay đổi giá trị của v thông qua con trỏ p
-	fmt.Println(v.X)
-
-	z := &Vertex{1, 2} // has type *Vertex
-	fmt.Println(z.X)
+	var a [2]string // Biến a là mảng string có 2 phần tử
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Print(a)
 }
