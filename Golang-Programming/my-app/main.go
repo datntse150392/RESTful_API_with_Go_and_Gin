3f71d5eec366292d93a3580bc7d2c5c1af81cac7
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-04-07 20:16:16 +0700

    Lập trình Golang #7: Concurrency với Goroutines, Channel, Select, Mutex (có demo)
    - Demo buffer channel

--- Golang-Programming/my-app/main.go
+++ Golang-Programming/my-app/main.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 func main() {
 	fmt.Println("Hello, World!")
-	// ch := make (chan int)
+	
+	// Khai báo channel unbuffered (không có buffer) 
+	// ch := make(chan int)
 
-	// VD1:
-	// fmt.Print("VD1:", <-ch)
+	// Khai báo channel buffered (có buffer)
+	// ch2 := make(chan int, 2) // Có thể chứa tối đa 2 phần tử
+	// ch2 <- 1
+	// ch2 <- 2
+	// ch2 <- 3 // Lỗi, vì đã đầy deadlock
 
-	// VD2: 
-	ch2 := make(chan int)
+	// Ví dụ về channel buffer
+	ch3 := make(chan int, 10)
 
-	go func () {
-		ch2 <- 1
+	go func() {
+		for i := 0; i < 100; i++ {
+			ch3 <- i
+			fmt.Print("Send: ", i, "\n")
+		}
 	}()
 
-	// ch2 <- 2
-	fmt.Print("VD2:", <-ch2)
+	for i := 0; i < 100; i++ {
+		fmt.Println(<-ch3)
+		time.Sleep(time.Second)
+	}
 
+	/*
+	Giải thích ví dụ trên
+		+ Do khi khai báo channel ch3 đã có buffer 10, tức là có thể nhận 10 slot giá trị trước khi bị block
+		+ Khi gửi giá trị vào channel ch3, nếu channel chưa đầy thì sẽ không bị block
+		+ Khi bắt đầu vòng lặp thứ 11 thì phải đợi lấy giá trị từ channel ch3, nếu channel chưa có giá trị thì sẽ bị block
+	*/
 }
