577e76ce3fd1e3cc00bddcaf90b591905d6510df
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-04-09 23:34:20 +0700

    Lập trình Golang #7: Concurrency với Goroutines, Channel, Select, Mutex (có demo)
    - Ở ví dụ 1, ta có thể thấy:
    + 2 channel được khai báo và chanel thứ 1 phải sleep 2s và channel thứ 2 chỉ sleep 1 nhưng mà khi in ra tuần tự và gặp phải lấy ra giá trị của ch1 thì phải đợi cho dù channel thứ 2 có kq sớm đi chăng nữa
    - Ở ví dụ thứ 2 là dùng select để giải quyết việc mà lấy giá trị cho channel nào sớm thì giải quyết sớm

--- Golang-Programming/my-app/main.go
+++ Golang-Programming/my-app/main.go
@@ -1,20 +1,57 @@
 package main
 
-func fibonacci(n int, c chan int) {
+import (
+	"time"
+)
+
+func fibonacci(c, quit chan int) {
 	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		c <- x
-		x, y = y, x+y
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			println("quit")
+			return
+		}
 	}
-	close(c)
+
 }
 
 func main() {
 
-	c := make(chan int, 10)
-	go fibonacci((cap(c)), c)
+	// Fx 1
+	ch1 := make(chan int)
+	ch2 := make(chan int)
 
-	for i := range c {
-		println(i)
+	go func (){
+		time.Sleep(2 * time.Second)
+		ch1 <- 1
+	}()
+
+	go func (){
+		time.Sleep(1 * time.Second)
+		ch2 <-2
+	}()
+
+	// fmt.Print("ch1", <-ch1)
+	// fmt.Print("ch2", <-ch2)	
+	select{
+	case v1 := <-ch1: 
+		println("ch1", v1)
+	case v2 := <-ch2:
+		println("ch2", v2)
 	}
+
+	// Fx2
+	c := make(chan int)
+	quit := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
 }
