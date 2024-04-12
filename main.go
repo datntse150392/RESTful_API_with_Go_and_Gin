9386442e57592059347d5ddf5196d508c1f13d19
datntse150392 <110538773+datntse150392@users.noreply.github.com>
2024-04-12 10:23:44 +0700

    Lập trình REST API với Golang #11: API Get item by Id

--- main.go
+++ main.go
@@ -8,17 +8,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
 type TodoItem struct {
-	Id          int        `json:"id"`
-	Title       string     `json:"title"`
-	Description string     `json:"description"`
-	Status      string     `json:"status"`
-	CreatedAt   *time.Time `json:"created_at"`
-	UpdatedAt   *time.Time `json:"updated_at"`
+	Id          int        `json:"id" gorm:"column:id;"`
+	Title       string     `json:"title" gorm:"column:title;"`
+	Description string     `json:"description" gorm:"column: description;"`
+	Status      string     `json:"status" gorm:"column: status;"`
+	CreatedAt   *time.Time `json:"created_at" gorm:"column:created_at;"`
+	UpdatedAt   *time.Time `json:"updated_at" gorm:"column: updated_at;"`
 }
+
+func (TodoItem) TableName() string {
+	return "todo_items"
+}
+
 type TodoItemCreation struct {
 	Id          int    `json:"-" gorm:"column:id;"`
 	Title       string `json:"title" gorm:"column:title"`
@@ -26,7 +32,7 @@ type TodoItemCreation struct {
 }
 
 func (TodoItemCreation) TableName() string {
-	return "todo_items"
+	return TodoItem{}.TableName()
 }
 
 func main() {
@@ -36,17 +42,6 @@ func main() {
 		log.Fatalln(err)
 	}
 	fmt.Println("Connected to DB", db)
-	now := time.Now().UTC()
-
-	item := TodoItem{
-		Id:          1,
-		Title:       "This is taks 1",
-		Description: "This is taks 2",
-		Status:      "Done",
-		CreatedAt:   &now,
-		UpdatedAt:   &now,
-	}
-
 	r := gin.Default()
 
 	//CRUD: Create, Read, Update, Delete
@@ -54,7 +49,7 @@ func main() {
 	// GET /v1/items (get list item) or /v1/items?page=1
 	// GET /v1/items/:id (get an item detail by id
 	// (PUT/PATCH) /v1/items/:id (update an item by id)
-	// DELETE /v1/items/:id (Delete an item by id)
+	// DELETE /v1/items/:id (Delete an item by id)S
 
 	v1 := r.Group("v1")
 	{
@@ -62,16 +57,11 @@ func main() {
 		{
 			items.POST("", CreateItem(db))
 			items.GET("")
-			items.GET("/:id")
+			items.GET("/:id", GetDetailItem(db))
 			items.PUT("/:id")
 			items.DELETE("/:id")
 		}
 	}
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": item,
-		})
-	})
 	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
@@ -96,6 +86,35 @@ func CreateItem(db *gorm.DB) func(*gin.Context) {
 
 		c.JSON(http.StatusCreated, gin.H{
 			"data": data,
+		})
+	}
+}
+
+func GetDetailItem(db *gorm.DB) func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		var data TodoItem
+
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": err.Error(),
+			})
+		}
+
+		data.Id = id
+		db.First(&data)
+
+		if err := db.First(&data).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"status": http.StatusOK,
+			"data":   data,
 		})
 	}
 }
